6784f5a89f2a53cc111a83a670ecc722e3da756a
Pearly <thanyapond.kum@gmail.com>
2025-05-04 19:17:39 +0800

    Initial commit or your commit message

--- src/index.js
+++ src/index.js
@@ -11,7 +11,19 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// index.js or App.js
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/firebase-messaging-sw.js')
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+}
+
+
 reportWebVitals();
+
